b5ddc308294ef3d7cdaae80fccd3c400a59325b4
Jemar23 <jemarjohnson116@icloud.com>
2022-10-11 14:30:29 -0400

    Jemar Solution

--- challenge/LinkedListCycle/Johnson_Jemar.java
+++ challenge/LinkedListCycle/Johnson_Jemar.java
@@ -1,11 +1,11 @@
 public class Solution {
     public boolean isCycle(LinkedList head) {
         LinkedList slow = head;
-        LinkList fast = head;
+        LinkedList fast = head;
 
         while (fast != null && fast.next != null) {
             slow = slow.next;
-            fast = fast.next;
+            fast = fast.next.next;
 
             if (slow == fast) {
                 return true;
